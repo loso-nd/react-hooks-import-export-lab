8d950361cd7e81b86dc5d7710d1e5cd93d075066
Nahum <losond8@gmail.com>
2021-06-11 04:42:23 -0500

    Starter webapp structure to create a portfolio.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,11 @@ function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <a href="#about">I'm another link!</a>
+      <a href="#">I am not DJ Khaled</a>
     </nav>
   );
 }
+
+
+export default NavBar;
